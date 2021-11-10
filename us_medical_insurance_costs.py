26535dc7ed6868d188b86823fe02c63957f50f8a
ValerioFrancisco <93736573+ValerioFrancisco@users.noreply.github.com>
2021-11-10 00:07:01 +0000

    finished data loading method

--- us_medical_insurance_costs.py
+++ us_medical_insurance_costs.py
@@ -7,7 +7,7 @@ class UsInsuranceData:
 	# Initializes lists that will hold the data
 	def __init__(self):
 		self.age = []
-		self.ex = []
+		self.sex = []
 		self.bmi = []
 		self.children = []
 		self.smoker = []
@@ -19,4 +19,19 @@ class UsInsuranceData:
 	
 	def load_data(self, file_name):
 		# TODO: finish this!
-		pass
+		with open(file_name, newline='') as csv_file:
+        		reader = csv.DictReader(csv_file)
+        		for row in reader:
+            			self.age.append(int(row['age']))
+            			self.sex.append(row['sex'])
+            			self.bmi.append(float(row['bmi']))
+            			self.children.append(int(row['children']))
+            			self.smoker.append(row['smoker'])
+            			self.region.append(row['region'])
+            			self.charges.append(float(row['charges']))
+
+
+data = UsInsuranceData()
+data.load_data('insurance.csv')
+
+print(data.age)
