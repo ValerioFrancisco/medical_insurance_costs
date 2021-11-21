d9216ce33c6ad791a466336d676844f5953d4200
ValerioFrancisco <93736573+ValerioFrancisco@users.noreply.github.com>
2021-11-21 22:25:20 +0000

    Update us_medical_insurance_costs.py

--- us_medical_insurance_costs.py
+++ us_medical_insurance_costs.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from collections import Counter
 import matplotlib.pyplot as plt
 import cmdmenu as cm
 
@@ -7,21 +8,26 @@ import cmdmenu as cm
 class UsInsuranceData:
 	# Initializes lists that will hold the data
 	def __init__(self, file_name):
-		self.data_df = pd.read_csv(file_name)
+		self.df = pd.read_csv(file_name)
 	
 	def __repr__(self):
 		return 'Represents data on US medical insurance costs'
 	
+	# Prints a summary of the data
 	def summary(self):
-		print(f'There are {self.data_df.shape[0]} records in the file.')
-		age_mean = self.data_df['age'].mean()
-		print(f'The average age is {round(age_mean, 2)} years old.')
-		bmi_mean = self.data_df['bmi'].mean()
-		print(f'The average bmi is {round(bmi_mean, 2)}')
-		charges_mean = self.data_df['charges'].mean()
-		print(f'The average insurance charges are US${round(charges_mean, 2)}')
-		# TODO: must check this out
-		# refresh pandas knowlege
+		print(f'There are {self.df.shape[0]} records in the file.')
+		age_mean = self.df['age'].mean()
+		print(f'The average age is {age_mean:.2f} years old.')
+		bmi_mean = self.df['bmi'].mean()
+		print(f'The average bmi is {bmi_mean:.2f}')
+		charges_mean = self.df['charges'].mean()
+		print(f'The average insurance charges are US${charges_mean:.2f}')
+		region_counter = Counter(self.df['region'])
+		for key in region_counter:
+			print(f'There are {region_counter[key]} persons from the {key}.')
+		children_counter = Counter(self.df['children'])
+		for key in children_counter:
+			print(f'{children_counter[key]} persons have {key} children')
 
 
 # Loads data from CSV file
