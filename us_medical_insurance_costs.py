14925063bc23dbc6081ad883209ba8063cae4bef
ValerioFrancisco <93736573+ValerioFrancisco@users.noreply.github.com>
2021-11-24 00:29:14 +0000

    Added smoker comparision graph

--- us_medical_insurance_costs.py
+++ us_medical_insurance_costs.py
@@ -6,15 +6,16 @@ from cmdmenu import CmdMenu
 # This class is responsible to handle the data load and analisys
 
 class UsInsuranceData:
-	# Initializes lists that will hold the data
+	# Initializes dataframe that will hold the data
 	def __init__(self, file_name):
 		self.df = pd.read_csv(file_name)
 	
 	def __repr__(self):
 		return 'Represents data on US medical insurance costs'
 	
-	# Prints a summary of the data
 	def summary(self):
+		# Prints a summary of the data
+		print('\n\t*** Data Summary ***')
 		print(f'There are {self.df.shape[0]} records in the file.')
 		age_mean = self.df['age'].mean()
 		print(f'The average age is {age_mean:.2f} years old.')
@@ -28,7 +29,21 @@ class UsInsuranceData:
 		children_counter = Counter(self.df['children'])
 		for key in children_counter:
 			print(f'{children_counter[key]} persons have {key} children')
+		smoker_count = Counter(self.df['smoker'])['yes']
+		print(f'{smoker_count} persons in the data are smokers')
 
+	
+	def smoker_comparision(self):
+		# Plots a bar graph comparing the insurance charges of smokers vs non smokers
+		smoker_mean = self.df.loc[self.df['smoker'] == 'yes']['charges'].mean()
+		print(f'Average smoker charges: {smoker_mean:.2f}')
+		non_smoker_mean = self.df.loc[self.df['smoker'] == 'no']['charges'].mean()
+		print(f'Average non smoker charges: {non_smoker_mean:.2f}')
+		plt.figure('Smoker comparision')
+		plt.bar(['Smokers', 'Non smokers'], [smoker_mean, non_smoker_mean])
+		plt.title('Smoker Vs Non smoker charges')
+		plt.ylabel('Average insurance charges')
+		plt.show()
 
 # Loads data from CSV file
 insurance = UsInsuranceData('insurance.csv')
@@ -37,5 +52,6 @@ insurance = UsInsuranceData('insurance.csv')
 # Initializes menu
 app = CmdMenu('US Medical Insurance Costs')
 app.add(insurance.summary, 'Data Summary')
+app.add(insurance.smoker_comparision, 'Smoker insurance charges')
 # run the application
 app.run()
